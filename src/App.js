baced102bb9409a7627ec2fb158770fe84a1f915
Y3fri <yeferguzman11@gmail.com>
2024-02-22 15:23:47 -0500

    pwa completa, para iphone como para android

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout, Home, About, Gallery } from "./components";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Layout>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="galeria" element={<Gallery />} />
+      <Route path="acerca" element={<About />} />
+    </Routes>
+  </Layout>
+  </Router>
   );
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+      <>
+        <main>
+          <h2>Welcome maifrend</h2>
+          <p>Navega un poco.</p>
+        </main>
+      </>
+    );
+  }
+  
