5fc95009e4eb9142241d743c0dcd424444f7bd74
Y3fri <yeferguzman11@gmail.com>
2024-05-31 14:23:06 -0500

    mejora recogida, vistas y direccion

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -13,17 +13,17 @@ const Home = () => {
             <nav className='navintro'>
                 <ul className="ulLayout">
                     <li className="liLayout">
-                        <Link className="aLayout iconLink" to="../Home">
+                        <Link className="aLayout iconLink" to="../">
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-question-mark" width="34" height="34" viewBox="0 0 24 24" strokeWidth="3.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M8 8a3.5 3 0 0 1 3.5 -3h1a3.5 3 0 0 1 3.5 3a3 3 0 0 1 -2 3a3 4 0 0 0 -2 4" />
                                 <path d="M12 19l0 .01" />
                             </svg>
                         </Link>
-                        <Link className="aLayout textLink" to="../Home">Inicio</Link>
+                        <Link className="aLayout textLink" to="../">Inicio</Link>
                     </li>
                     <li className="liLayout">
-                        <Link className="aLayout iconLink" to="../">
+                        <Link className="aLayout iconLink" to="../recicla">
                             <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-home" width="34" height="34" viewBox="0 0 24 24" strokeWidth="3.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                 <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
@@ -31,7 +31,7 @@ const Home = () => {
                                 <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
                             </svg>
                         </Link>
-                        <Link className="aLayout textLink" to="../">Donar</Link>
+                        <Link className="aLayout textLink" to="../recicla">Reciclar</Link>
                     </li>
                     <li className="liLayout">
                         <Link className="aLayout iconLink" to="../productos">
